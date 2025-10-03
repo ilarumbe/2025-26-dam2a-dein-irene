dcb4afa915f156fc3d6553c817ad1f2c279cb424
ilarumbe <ilarumbzoz11@educacion.navarra.es>
2025-10-03 12:29:02 +0200

    Archivo agregado

---
+++ 1.6_Window_y_Paneles_Irene_Larumbe/1.6_Window_y_Paneles_Irene_Larumbe/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace _1._6_Window_y_Paneles_Irene_Larumbe
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
