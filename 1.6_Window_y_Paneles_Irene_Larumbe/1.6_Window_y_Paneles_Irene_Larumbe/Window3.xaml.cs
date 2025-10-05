e0b4d11ffffa0316ef300ee6ee9e34a9f3b92992
ilarumbe <ilarumbzoz11@educacion.navarra.es>
2025-10-05 23:33:18 +0200

    Ventanas agregadas y funcionales

---
+++ 1.6_Window_y_Paneles_Irene_Larumbe/1.6_Window_y_Paneles_Irene_Larumbe/Window3.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace _1._6_Window_y_Paneles_Irene_Larumbe
+{
+    /// <summary>
+    /// Lógica de interacción para Window3.xaml
+    /// </summary>
+    public partial class Window3 : Window
+    {
+        public Window3()
+        {
+            InitializeComponent();
+        }
+        private void btnEntrar_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
