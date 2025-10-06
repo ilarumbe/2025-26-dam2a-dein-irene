7718d03fa4b988e2fa42e77a14802dfafd329b85
ilarumbe <ilarumbzoz11@educacion.navarra.es>
2025-10-06 10:48:53 +0200

    Proyecto 1.8 creado

---
+++ A1.8_Controles_Irene_Larumbe/A1.8_Controles_Irene_Larumbe/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace A1._8_Controles_Irene_Larumbe
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
