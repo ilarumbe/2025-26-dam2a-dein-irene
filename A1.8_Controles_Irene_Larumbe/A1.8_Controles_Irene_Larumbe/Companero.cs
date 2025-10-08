24bbba3affdafa56b78daa95939d3f26689810a0
ilarumbe <ilarumbzoz11@educacion.navarra.es>
2025-10-08 19:41:11 +0200

    A1.8 acabada

---
+++ A1.8_Controles_Irene_Larumbe/A1.8_Controles_Irene_Larumbe/Companero.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A1._8_Controles_Irene_Larumbe
+{
+    public class Companero
+    {
+        public int ID { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido1 { get; set; }
+        public string Apellido2 { get; set; }
+    }
+}
