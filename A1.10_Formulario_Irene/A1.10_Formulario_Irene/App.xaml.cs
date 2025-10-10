08fce023436b2ba9971cf323dee3ed8e9193c503
ilarumbe <ilarumbzoz11@educacion.navarra.es>
2025-10-10 13:07:17 +0200

    Casi acabada actividad A1.10

---
+++ A1.10_Formulario_Irene/A1.10_Formulario_Irene/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace A1._10_Formulario_Irene
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
