778d5c190c67f22771e11c5b23ac486a19e2e749
ilarumbe <ilarumbzoz11@educacion.navarra.es>
2025-10-16 10:55:11 +0200

    Empezando A11

---
+++ A1.11_Orla_Irene/A1.11. Aplicación_Orla_de_clase/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace A1._11._Aplicación_Orla_de_clase
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
