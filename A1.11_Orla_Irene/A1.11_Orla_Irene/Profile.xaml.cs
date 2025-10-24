2549d834d1a826f08d2744f73bfc620243934177
ilarumbzoz1 <ilarumbzoz11@educacion.navarra.es>
2025-10-24 13:22:06 +0200

    agregada A11 porque se habia eliminado

---
+++ A1.11_Orla_Irene/A1.11_Orla_Irene/Profile.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+using String = System.String;
+
+namespace A1._11_Orla_Irene
+{
+    public partial class Profile : Window
+    {
+        public Profile(String Nombre, String Apellidos, String Email, String Foto)
+        {
+            InitializeComponent();
+            Profile_Nombre.Text = Nombre;
+            Profile_Apellidos.Text = Apellidos;
+            Profile_Email.Text = Email;
+            Profile_Image.Source = new BitmapImage(new Uri(string.Format(@"..\..\{0}", Foto), UriKind.Relative));
+        }
+
+<<<<<<< HEAD
+        
+
+=======
+>>>>>>> 782d07fee129b4e0d04b5f3cc0f0ea045f650b64
+    }
+}
