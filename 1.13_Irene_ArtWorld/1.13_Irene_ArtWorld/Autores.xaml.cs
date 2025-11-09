5146dc1c2e9a311eedd6e7b217874fc8ca0903e9
ilarumbe <ilarumbzoz11@educacion.navarra.es>
2025-11-09 22:46:18 +0100

    Ya he hecho que funcione lo de que aparezca y desaparezca la descripcion de los autores cuando pasas el raton por encima

--- 1.13_Irene_ArtWorld/1.13_Irene_ArtWorld/Autores.xaml.cs
+++ 1.13_Irene_ArtWorld/1.13_Irene_ArtWorld/Autores.xaml.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Configuration;
 using System.Data.SqlClient;
 using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
 
 namespace _1._13_Irene_ArtWorld
 {
@@ -21,6 +23,68 @@ namespace _1._13_Irene_ArtWorld
             Menu menu = new Menu();
             menu.Show();
             this.Close();
+        }
+
+        private void Autor_MouseEnter(object sender, MouseEventArgs e)
+        {
+            if (sender is Border border)
+            {
+                if (border.Child is Grid grid)
+                {
+                    grid.MinHeight = grid.ActualHeight;
+                    grid.MinWidth = grid.ActualWidth;
+
+                    var cuadrosPanel = FindChildByPartialName(grid, "CuadrosPanel") as UIElement;
+                    var descripcion = FindChildByPartialName(grid, "Descripcion") as UIElement;
+
+                    if (cuadrosPanel != null && descripcion != null)
+                    {
+                        cuadrosPanel.Visibility = Visibility.Collapsed;
+                        descripcion.Visibility = Visibility.Visible;
+                    }
+                }
+            }
+        }
+
+        private void Autor_MouseLeave(object sender, MouseEventArgs e)
+        {
+            if (sender is Border border)
+            {
+                if (border.Child is Grid grid)
+                {
+                    grid.ClearValue(Grid.MinHeightProperty);
+                    grid.ClearValue(Grid.MinWidthProperty);
+
+                    var cuadrosPanel = FindChildByPartialName(grid, "CuadrosPanel") as UIElement;
+                    var descripcion = FindChildByPartialName(grid, "Descripcion") as UIElement;
+
+                    if (cuadrosPanel != null && descripcion != null)
+                    {
+                        cuadrosPanel.Visibility = Visibility.Visible;
+                        descripcion.Visibility = Visibility.Collapsed;
+                    }
+                }
+            }
+        }
+
+        private FrameworkElement FindChildByPartialName(FrameworkElement parent, string partialName)
+        {
+            if (parent == null) return null;
+
+            foreach (object child in LogicalTreeHelper.GetChildren(parent))
+            {
+                if (child is FrameworkElement fe)
+                {
+                    if (fe.Name.Contains(partialName))
+                        return fe;
+
+                    var found = FindChildByPartialName(fe, partialName);
+                    if (found != null)
+                        return found;
+                }
+            }
+
+            return null;
         }
     }
 }
