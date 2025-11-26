656fd8597983a4ab7811cc7bb7e58e4ce3039d40
ilarumbzoz1 <ilarumbzoz11@educacion.navarra.es>
2025-11-26 08:58:30 +0100

    He creado la estructura general del proyecto, he hecho la clase Libro y el LibroRepository

---
+++ P2526-Irene-Biblioteca/P2526-Irene-Biblioteca/Models/Autor.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P2526_Irene_Biblioteca.Models
+{
+    public class Autor
+    {
+    }
+}
---
+++ P2526-Irene-Biblioteca/P2526-Irene-Biblioteca/Models/Libro.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P2526_Irene_Biblioteca.Models
+{
+    public class Libro
+    {
+        public int IdLibro { get; set; }
+        public string Titulo { get; set; }
+        public int Ano { get; set; }
+
+        public int IdAutor { get; set; }
+        public int IdCategoria { get; set; }
+
+        public int Stock { get; set; }
+
+        public Autor Autor { get; set; }
+        public Categoria Categoria { get; set; }
+    }
+}
