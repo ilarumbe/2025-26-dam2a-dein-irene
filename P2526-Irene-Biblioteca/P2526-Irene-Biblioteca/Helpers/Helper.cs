51d0fe89238f6c42a9aee674b35dc6b60d1ae7b0
ilarumbzoz1 <ilarumbzoz11@educacion.navarra.es>
2025-11-27 10:52:40 +0100

    He hecho el helper.cs

--- P2526-Irene-Biblioteca/P2526-Irene-Biblioteca/Helpers/Helper.cs
+++ P2526-Irene-Biblioteca/P2526-Irene-Biblioteca/Helpers/Helper.cs
@@ -3,10 +3,35 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace P2526_Irene_Biblioteca.Helpers
 {
-    internal class Helper
+    public class Helper
     {
+        private readonly Action<object> execute;
+        private readonly Predicate<object> canExecute;
+
+        public Helper(Action<object> execute, Predicate<object> canExecute = null)
+        {
+            this.execute = execute ?? throw new ArgumentNullException(nameof(execute));
+            this.canExecute = canExecute;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return canExecute == null || canExecute(parameter);
+        }
+
+        public void Execute(object parameter)
+        {
+            execute(parameter);
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add => CommandManager.RequerySuggested += value;
+            remove => CommandManager.RequerySuggested -= value;
+        }
     }
 }
